e60e889c1a782c1b6acdc854afd2029ec35dfc69
emanuele <emanuele@pc-emanuele>
2015-03-25 17:56:11 +0100

    [issue #2] <classe user>

--- proloco-domain-model/src/main/java/it/proloco/dm/User.java
+++ proloco-domain-model/src/main/java/it/proloco/dm/User.java
@@ -2,4 +2,79 @@ package it.proloco.dm;
 
 public class User {
 
+	String username;
+	int userType;
+	String email;
+	String password;
+	String name;
+	String surname;
+	String addres;
+	String description;
+	String VAT;
+	//@santini stefano partita iva se l'utenteè titolare di un azienda. per fare pubblicita.
+	String cellNumber;
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public int getUserType() {
+		return userType;
+	}
+	public void setUserType(int userType) {
+		this.userType = userType;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	public String getAddres() {
+		return addres;
+	}
+	public void setAddres(String addres) {
+		this.addres = addres;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getVAT() {
+		return VAT;
+	}
+	public void setVAT(String vAT) {
+		VAT = vAT;
+	}
+	public String getCellNumber() {
+		return cellNumber;
+	}
+	public void setCellNumber(String cellNumber) {
+		this.cellNumber = cellNumber;
+	}
+	
+	
+	
 }

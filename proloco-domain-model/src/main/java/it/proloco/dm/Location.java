ab923b8940583de21d1f87b0b8638605aa4196fe
Ofordo <gazly3@gmail.com>
2015-03-27 18:05:57 +0100

    Aggiunte le proprietà della classe location

--- proloco-domain-model/src/main/java/it/proloco/dm/Location.java
+++ proloco-domain-model/src/main/java/it/proloco/dm/Location.java
@@ -1,7 +1,38 @@
 package it.proloco.dm;
 
-public class Location {
+
+public class Location extends GeoEntity
+
+{
+	
 	String name;
+    String image;
+    String Description;
+    String howToReachUs;
+    
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public String getDescription() {
+		return Description;
+	}
+
+	public void setDescription(String description) {
+		Description = description;
+	}
+
+	public String getHowToReachUs() {
+		return howToReachUs;
+	}
+
+	public void setHowToReachUs(String howToReachUs) {
+		this.howToReachUs = howToReachUs;
+	}
 
 	public String getName() {
 		return name;
